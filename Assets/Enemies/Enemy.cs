f5e28c6cf82182e277b1cac6d9f7b2c1518612da
Zachary Mcilwain <Zacharyamcilwain@gmail.com>
2018-07-26 13:26:46 -0700

    Improved level and added simple enemy ai movement

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets.Characters.ThirdPerson;
 using UnityEngine;
 
  class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100;
+    [SerializeField] float attackRadius = 4f;
 
     float currentHealthPoints = 100f;
+    GameObject player = null;
+    AICharacterControl aiCharacterControl = null;
 
     public float healthAsPercentage
     {
         get
         {
             return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
